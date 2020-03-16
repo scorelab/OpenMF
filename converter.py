a155de4dd1085315ed4ebfa7b7246cff96bc728f
shivanshu <shivanshu1333@gmail.com>
2020-03-16 23:18:23 +0530

    modified converter.py to add support for json

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 import sys
-from scripts.file_helper import write_as_tsv
+from scripts.file_helper import *
 from utils import ROOT_DIR
 
 
@@ -17,53 +17,40 @@ def get_os_based_sqlite():
     elif os_check == 'darwin':
         return mac_tool
 
-
-def convert_to_tsv(db_path, tsv_file_path):
-    connection = sqlite3.connect(db_path)
-    cursor = connection.cursor()
-    cursor.execute('SELECT name FROM sqlite_master WHERE type = "table"')
-    tables = []
-    print('Tables in database are :: ')
-    for table_tuple in cursor.fetchall():
-        tables.append(table_tuple[0])
-        print(table_tuple[0])
-    for table in tables:
-        schema_query = 'pragma table_info("{0}")'.format(table)
-        schema = cursor.execute(schema_query).fetchall()
-        columns = []
-        for columns_tpl in schema:
-            columns.append(columns_tpl[1])
-        select_query = 'SELECT * from {0};'.format(table)
-        rows = cursor.execute(select_query).fetchall()
-        write_as_tsv(tsv_file_path + '/' + table + '.tsv', columns, rows)
-
-
 def print_help():
     help_str = """
         converter is a simple script to convert DB files to tsv (tab separated value)
         python converter.py <src-file-path> <destination-folder>
         example : 
-            python converter.py /home/user/Downloads/accounts.db /home/user/data/
+            python converter.py /home/user/Downloads/accounts.db /home/user/data/ json or tsv
     """
     print(help_str)
 
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) < 3:
+    if len(args) < 4:
         print('Insufficient arguments')
         print_help()
-    elif len(args) == 3:
+    elif len(args) == 4:
         src_path = args[1]
         dest_dir = args[2]
+        file_type = args[3]
         if not os.path.exists(src_path):
             print('Given db file path is incorrect. This filepath doesn\'t exists ::' + src_path)
         case_name = src_path.split("/")[-3]
         folder_name = os.path.splitext(os.path.basename(src_path))[0]
         file_dir = dest_dir+"/"+case_name+"/"+folder_name
-        if not os.path.exists(file_dir):
-            os.makedirs(file_dir, exist_ok=True)
-        convert_to_tsv(src_path, file_dir)
+        if file_type == 'json':
+            file_dir = file_dir+'/'+'json'
+            if not os.path.exists(file_dir):
+                os.makedirs(file_dir, exist_ok=True)
+            convert_to_json(src_path, file_dir)
+        elif file_type == 'tsv':
+            file_dir = file_dir+'/'+'tsv'
+            if not os.path.exists(file_dir):
+                os.makedirs(file_dir, exist_ok=True)
+            convert_to_tsv(src_path, file_dir)
         pass
     else:
         print('Incorrect no. of arguments passed')
