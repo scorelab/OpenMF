a0783d3f35490f269f3994d6e72cd79a05eea459
Amlan <amlannandy5@gmail.com>
2021-02-27 10:31:16 +0530

    Added Private Route

--- React-frontend/src/App.js
+++ React-frontend/src/App.js
@@ -5,21 +5,22 @@ import { Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage'
-import Alert from './components/core/Alert'
+import ContactPage from './pages/ContactPage';
+import Alert from './components/core/Alert';
 
 import store from './store/store';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="containerBI">
+      <div className='containerBI'>
         <Alert />
         <Switch>
-          <Route path="/login" exact component={LoginPage} />
-          <Route path='/' exact component={HomePage} />
-          <Route path='/about' exact component={AboutPage} />
-          <Route path='/contact' exact component={ContactPage} />
+          <Route path='/login' exact component={LoginPage} />
+          <PrivateRoute path='/' exact component={HomePage} />
+          <PrivateRoute path='/about' exact component={AboutPage} />
+          <PrivateRoute path='/contact' exact component={ContactPage} />
         </Switch>
       </div>
     </Provider>
