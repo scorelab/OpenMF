579eab5a39b55561d39ffacc2369e924f17a63bd
Amlan Kumar Nandy <amlannandy5@gmail.com>
2021-03-09 13:41:37 +0530

    fixed case routes

--- flask-backend/api/routes/case.py
+++ flask-backend/api/routes/case.py
@@ -12,16 +12,22 @@ cases_schema = CaseSchema(many=True)
 
 case = Blueprint('case', __name__, url_prefix='/case')
 
+dirname = os.path.dirname(__file__)
+cases_data_path = os.path.join(dirname, '../../../data/')
+
+
 @case.route('/count', methods=["GET"])
 def count():
-    return jsonify({'status':200,
-                    'total_users':Case.query.count()})
+    return jsonify({'status': 200,
+                    'total_users': Case.query.count()})
+
 
 @case.route('/list', methods=["GET"])
 def list():
     all_cases = Case.query.order_by(Case.timestamp).all()
     result = cases_schema.dump(all_cases)
     return jsonify(result)
+
 
 @case.route('/delete', methods=['POST'])
 def deletecase():
@@ -41,25 +47,50 @@ def deletecase():
     db.session.commit()
     return 'case deleted', 202
 
+
 @case.route('/open/<case_name>', methods=["GET"])
 def openCase(case_name):
-    os.chdir('../../..')
-    path = os.getcwd()+'/data/'+case_name
-    os.chdir(ROOT_DIR)
-    files = os.listdir(path)
-    return files
+    try:
+        os.chdir('../../..')
+        path = cases_data_path + case_name
+        os.chdir(ROOT_DIR)
+        files = os.listdir(path)
+    except FileNotFoundError as err:
+        response = {
+            'success': False,
+            'msg': 'Case with that name does not exist',
+        }
+        return jsonify(response), 404
+    response = {
+        'success': True,
+        'data': files,
+    }
+    return jsonify(response), 200
+
 
 @case.route('/list-files/<case_name>/<folder_name>', methods=["GET"])
 def openFolder(case_name, folder_name):
-    os.chdir('../../..')
-    path = os.getcwd()+'/data/'+case_name+'/'+folder_name
-    os.chdir(ROOT_DIR)
-    files = os.listdir(path)
-    return files
+    try:
+        os.chdir('../../..')
+        path = cases_data_path + case_name + '/' + folder_name
+        os.chdir(ROOT_DIR)
+        files = os.listdir(path)
+    except FileNotFoundError:
+        response = {
+            'success': False,
+            'msg': 'Folder does not exist',
+        }
+        return jsonify(response), 404
+    response = {
+        'success': True,
+        'data': files,
+    }
+    return jsonify(response), 200
+
 
 @case.route('/list-files/<case_name>/<folder_name>/<file_name>', methods=["GET"])
 def openFile(case_name, folder_name, file_name):
     os.chdir('../../..')
-    File = os.getcwd()+'/data/'+case_name+'/'+folder_name+'/'+file_name
+    file = cases_data_path + case_name + '/' + folder_name + '/' + file_name
     os.chdir(ROOT_DIR)
-    return File
+    return file
