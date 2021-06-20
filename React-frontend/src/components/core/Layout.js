01f7bf94da5c262bf719a7e4cce92f38e155c7ca
sachin <sachinsom507@gmail.com>
2021-06-19 17:51:36 -0700

    Modify footer, layout, navbar components

--- React-frontend/src/components/core/Layout.js
+++ React-frontend/src/components/core/Layout.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Footer from './Footer';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import Sidebar from '../Sidebar';
-import '../../App.css';
+import Footer from './Footer';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Grid
+} from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: 0,
+    backgroundColor: '#f4f6f8',
+    height: '100vh',
+    overflow: 'auto'
+  }
+}))
 
-const Layout = ({ children, sidebarBool = true, background = true }) => {
+// Display main content of layout
+export default function LayoutMain ({ children, sidebarBool=true, background=true}) {
+  const classes = useStyles()
+
   return (
-    <div className='containerBI'>
-      <Navbar />
-      <MDBRow className={!background ? 'bg-white' : null}>
-        <MDBCol md='2'>{sidebarBool && <Sidebar />}</MDBCol>
-        <MDBCol md='10'>
-          <MDBContainer>{children}</MDBContainer>
-        </MDBCol>
-      </MDBRow>
-      <Footer />
-    </div>
-  );
-};
-
-export default Layout;
+    <>
+      <main className={classes.content}>
+        <Navbar />
+        <Grid container>
+        {
+          (sidebarBool) ?
+          <>
+            <Grid item xs={2} sm={2} md={2} lg={2}>
+              <Sidebar />
+            </Grid>
+            <Grid item xs={10} sm={10} md={10} lg={10}>
+              {children}
+            </Grid>
+          </>:
+          <>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              {children}
+            </Grid>
+          </>
+        }
+        </Grid>
+        <Footer />
+      </main>
+    </>
+  )
+}
--- React-frontend/src/components/core/Navbar.js
+++ React-frontend/src/components/core/Navbar.js
@@ -1,60 +1,304 @@
-import React from 'react';
-import Logo from '../../images/logo.png';
-import Ham from '../../images/hamburger.png';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import {
-  MDBDropdown,
-  MDBDropdownToggle,
-  MDBDropdownMenu,
-  MDBDropdownItem,
-} from 'mdbreact';
-import { MDBBtn } from 'mdbreact';
+import React from 'react'
 
-function Nvb() {
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  Link,
+  Avatar,
+  ListItemText,
+  IconButton,
+  Menu,
+  Fade,
+  MenuItem
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../store/actions/auth';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    height: '10vh',
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    backgroundColor: '#000',
+    color: '#fff',
+    position: 'fixed',
+  },
+  toolbar: {
+    flexWrap: 'wrap'
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+    fontWeight: 'bold'
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+    color: '#fff',
+    '&:hover': {
+      TextDecoration: 'none',
+      color: '#fff'
+    }
+  },
+  button: {
+    marginRight: theme.spacing(0.7),
+    color: '#fff',
+    borderRadius: theme.spacing(3.7),
+    borderColor: theme.palette.primary.extraLight,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.extraLight,
+      color: '#fff'
+    }
+  },
+  small: {
+    position: 'relative',
+    width: theme.spacing(3.7),
+    height: theme.spacing(3.7),
+    marginRight: theme.spacing(5),
+    '&::after': {
+      position: 'absolute',
+      content: '" "',
+      width: theme.spacing(8),
+      height: theme.spacing(8),
+      backgroundImage: `url(${logo})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'contain',
+      backgroundRepeat:'no-repeat',
+      zIndex: 999,
+    },
+    '&:onFocus': {
+      border: 'none',
+      outline: 'none'
+    }
+  },
+  purple: {
+    width: theme.spacing(3.75),
+    height: theme.spacing(3.75),
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+    fontSize: '17px'
+  }
+}))
+
+// Common navbar for Dashboard, Home, Simulator, Gallery, etc.
+export function Header () {
+  const history = useHistory()
+  const classes = useStyles()
+  const dispatch = useDispatch()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const auth = useSelector(state => state.auth)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <div>
-      <Navbar expand="lg" bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Link class="navbar-brand" to="/">
-            <img src={Logo} height="50" width="50" alt="logo"></img>
-            OpenMF
-          </Link>
-        </Nav>
-        <Nav>
-          <a href="https://github.com/scorelab/OpenMF/">
-            <MDBBtn outline color="success">
-              Help
-            </MDBBtn>
-          </a>
-          <Link to="/login">
-            <MDBBtn outline color="primary">
-              Login
-            </MDBBtn>
-          </Link>
-          <MDBDropdown>
-            <MDBDropdownToggle caret color="dark">
-              {
-                <img
-                  className="thumbnail-image"
-                  src={Ham}
-                  alt="down"
-                  height="25"
-                  width="28"
-                />
-              }
-            </MDBDropdownToggle>
-            <MDBDropdownMenu className="dropdown-default">
-              <MDBDropdownItem href="#!">Contact Admin</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Request Role</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Contribute</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Report bug</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        </Nav>
-      </Navbar>
-    </div>
-  );
+    <>
+      {/* Display logo */}
+      <IconButton edge="start" className={classes.button} color="primary">
+        <Link to="/" component={RouterLink}>
+          <div className={classes.small}></div>
+        </Link>
+      </IconButton>
+      <Typography
+        variant="h6"
+        color="inherit"
+        noWrap
+        className={classes.toolbarTitle}
+      >
+        <Link color="inherit" to="/" component={RouterLink}>
+          OpenMF
+        </Link>
+      </Typography>
+
+      {/* Display relative link to other pages */}
+      <nav>
+        {
+          (auth.isAuthenticated
+            ? (<>
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/"
+                component={RouterLink}
+                className={classes.link}
+              >
+                Home
+              </Link>
+
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/profile"
+                component={RouterLink}
+                className={classes.link}
+              >
+                Profile
+              </Link>
+
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/members"
+                component={RouterLink}
+                className={classes.link}
+              >
+                Members
+              </Link>
+
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/contact-us"
+                component={RouterLink}
+                className={classes.link}
+              >
+                Contact us
+              </Link>
+
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/dashboard"
+                component={RouterLink}
+                className={classes.link}
+              >
+                Dashboard
+              </Link>
+            </>)
+            : (<>
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/"
+                component={RouterLink}
+                style={{ marginRight: '20px' }}
+                className={classes.link}
+              >
+                Home
+              </Link>
+
+              <Link
+                variant="button"
+                color="textPrimary"
+                to="/https://github.com/scorelab/OpenMF/"
+                component={RouterLink}
+                style={{ marginRight: '20px' }}
+                className={classes.link}
+              >
+                Help
+              </Link>
+            </>
+            )
+          )
+        }
+      </nav>
+
+      {/* Display login option or user menu as per authenticated status */}
+      {
+        (!auth.isAuthenticated ? (
+        <>
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/login"
+            color="primary"
+            className={classes.button}
+            variant="outlined"
+          >
+            Login
+          </Button>
+
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/register"
+            color="primary"
+            variant="outlined"
+            className={classes.button}
+          >
+            Register
+          </Button>
+        </>)
+          : (<>
+
+            <IconButton
+              edge="start"
+              style={{ marginLeft: 'auto' }}
+              color="primary"
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <Avatar className={classes.purple}>
+                {(auth && auth.user) ? auth.user.name.charAt(0).toUpperCase(): (<span>wait...</span>)}
+              </Avatar>
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+              style={{ marginTop: '25px' }}
+            >
+              <MenuItem
+                component={RouterLink}
+                to="/dashboard"
+                onClick={handleClose}
+              >
+                <ListItemText primary={auth && auth.user && auth.user.email}/>
+              </MenuItem>
+              <MenuItem
+                component={RouterLink}
+                to="/dashboard/profile"
+                onClick={handleClose}
+              >
+                My Profile
+              </MenuItem>
+              <MenuItem
+                component={RouterLink}
+                to="/dashboard"
+                onClick={handleClose}
+              >
+                Dashboard
+              </MenuItem>
+              <MenuItem onClick={() => {
+                dispatch(logout(history))
+              }}>
+                Logout
+              </MenuItem>
+            </Menu>
+          </>
+          )
+        )
+      }
+    </>
+  )
 }
 
-export default Nvb;
+export default function Navbar () {
+  const classes = useStyles()
+
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      className={classes.appBar}
+    >
+      <Toolbar variant="dense" color="default" className={classes.toolbar}>
+
+        <Header />
+      </Toolbar>
+    </AppBar>
+  )
+}
