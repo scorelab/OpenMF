68b931811e794f4ee43a866426f0f96d861b487d
sachin <sachinsom507@gmail.com>
2021-07-10 07:36:32 -0700

    Update theme.js

--- React-frontend/src/theme.js
+++ React-frontend/src/theme.js
@@ -1,9 +1,9 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
 
 // Custom material ui theme for this application
-const theme = createMuiTheme({
+const theme = createTheme({
     palette: {
         primary: {
             main: "#264d2c",
