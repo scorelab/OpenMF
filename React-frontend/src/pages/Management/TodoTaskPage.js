f995c9175d1ba8eb3d0eef12b6c79823032f8d6c
sachin <sachinsom507@gmail.com>
2021-07-19 09:17:48 -0700

    [Frontend]: Updated TodoTask and CompletedTaskPage for extractor user.

--- React-frontend/src/pages/Management/TodoTaskPage.js
+++ React-frontend/src/pages/Management/TodoTaskPage.js
@@ -8,19 +8,31 @@ import Layout from '../../components/core/Layout';
 import HomeLogo from '../../components/core/HomeLogo';
 import { useSelector } from 'react-redux';
 import TodoTasks from '../../components/Management/TodoTasks';
+import ExtractorLayout from '../../components/Extractor/ExtractorLayout';
 const TodoTaskPage = () => {
 
     // auth reducer
     const auth = useSelector(state => state.auth)
 
-
-    if(auth && auth.isAuthenticated){
+    // Check for managemnet user
+    if(auth && auth.isAuthenticated && auth.user && auth.user.role === "management"){
         return (
             <ManagementLayout sidebarBool={true}>
                 <TodoTasks />
             </ManagementLayout>
         )
     }
+
+    // Check for extractor user
+    else if(auth && auth.isAuthenticated && auth.user && auth.user.role === "extractor")
+    {
+        return (
+            <ExtractorLayout sidebarBool={true}>
+                <TodoTasks />
+            </ExtractorLayout>
+        )
+    }
+
     return (
         <Layout sidebarBool={false}>
             <HomeLogo />
