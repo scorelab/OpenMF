da55bb5fe08fa6040766db81c093572db1193f58
Swapnal <swapnalsahil@gmail.com>
2021-08-03 22:42:58 +0530

    common location for data visualization

--- flask-backend/api/routes/commonreport.py
+++ flask-backend/api/routes/commonreport.py
@@ -54,6 +54,40 @@ def get_common_call_report(case1, case2):
     return call_report
 
 
+def get_common_coordinates(case1, case2):
+    '''
+        get_common_coordinates() is responsible for 
+        fetching common location details between two cases.
+    '''
+
+    common_coordinates = []
+    coordinate_one = []
+    coordinate_two = []
+
+    with open(case1, 'r') as first:
+        first = csv.reader(first, delimiter='\t')
+        for row in first:
+
+            data = (row[4], row[5], row[7])
+            coordinate_one.append(data)
+
+    coordinate_one.remove(coordinate_one[0])
+
+    with open(case2, 'r') as second:
+        second = csv.reader(second, delimiter='\t')
+        for row in second:
+
+            data = (row[4], row[5], row[7])
+            coordinate_two.append(data)
+
+    coordinate_two.remove(coordinate_two[0])
+
+    common_coordinates = list(
+        set(coordinate_one).intersection(set(coordinate_two)))
+
+    return common_coordinates
+
+
 '''
     -------- APIs ------------
     
@@ -68,12 +102,26 @@ def get_common_call_report(case1, case2):
         "case_two": "case2"
     }
 
+    2. http://127.0.0.1:5000/commonreport/coordinates
+
+    This API is responsible for getting common
+    location details between two cases.
+    
+    method = ['POST']
+    {
+        "case_one": "case1",
+        "case_two": "case2"
+    }
 
 '''
 
 
 @commonreport.route('/calls', methods=['POST'])
 def calls():
+    '''
+    This API is responsible for getting common
+    contact details between two cases.
+    '''
     try:
         req = request.get_json()
 
@@ -112,6 +160,57 @@ def calls():
 
     if commoncalls:
         return jsonify(commoncalls)
+
+    else:
+        return "No Data Found", 404
+
+
+@commonreport.route('/coordinates', methods=['POST'])
+def commoncoordinates():
+    '''
+    This API is responsible for getting common
+    location details between two cases.
+    '''
+    try:
+        req = request.get_json()
+
+        case_one = str(req['case_one'])
+
+        case_two = str(req['case_two'])
+
+        case_one = Case.query.filter_by(case_name=case_one).first()
+
+        case_two = Case.query.filter_by(case_name=case_two).first()
+
+        if not case_one:
+            '''
+            If case one is not present in database.
+            '''
+            return 'case one with that name does not exist', 404
+
+        if not case_two:
+            '''
+            If case two is not present in database.
+            '''
+            return 'case two with that name does not exist', 404
+
+        case_one_path = case_one.data_path
+
+        case_two_path = case_two.data_path
+
+    except Exception as e:
+
+        print(e)
+
+        return 'Please provide valid Case', 400
+
+    case_one_path = os.sep.join([case_one_path, PATH_TO_LOCATION])
+    case_two_path = os.sep.join([case_two_path, PATH_TO_LOCATION])
+
+    coordinate_list = get_common_coordinates(case_one_path, case_two_path)
+
+    if coordinate_list:
+        return jsonify(coordinate_list)
 
     else:
         return "No Data Found", 404
