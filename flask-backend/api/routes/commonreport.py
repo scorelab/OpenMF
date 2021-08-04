9b3e8ebff32c33c669e4fb028efdf317957331ef
Swapnal <swapnalsahil@gmail.com>
2021-08-04 11:46:49 +0530

    common searched history with frequencies

--- flask-backend/api/routes/commonreport.py
+++ flask-backend/api/routes/commonreport.py
@@ -88,6 +88,32 @@ def get_common_coordinates(case1, case2):
     return common_coordinates
 
 
+def get_common_browser_history(Case1, Case2):
+    '''
+        get_common_browser_history() is responsible for
+        fetching common search history from browser.
+    '''
+    common_history = []
+
+    open_path_file = open(Case2, "r", encoding="UTF-8")
+
+    history_2 = open_path_file.read()
+
+    open_path_file.close()
+
+    with open(Case1, 'r', encoding="UTF-8") as first:
+        first = csv.reader(first, delimiter='\t')
+        for row in first:
+
+            data = (row[2])
+            if data in history_2:
+                common_history.append((data,row[3]))
+
+    common_history.remove(common_history[0])
+    common_history = list(set(common_history))
+    
+    return common_history
+
 '''
     -------- APIs ------------
     
@@ -113,6 +139,16 @@ def get_common_coordinates(case1, case2):
         "case_two": "case2"
     }
 
+    3. http://127.0.0.1:5000/commonreport/browser
+
+    This API is responsible for fetching common browser
+    searched history details between two cases.
+    
+    method = ['POST']
+    {
+        "case_one": "case1",
+        "case_two": "case2"
+    }
 '''
 
 
@@ -211,6 +247,57 @@ def commoncoordinates():
 
     if coordinate_list:
         return jsonify(coordinate_list)
+
+    else:
+        return "No Data Found", 404
+
+
+@commonreport.route('/browser', methods=['POST'])
+def commonhistory():
+    '''
+    This API is responsible for getting common
+    browser history details between two cases.
+    '''
+    try:
+        req = request.get_json()
+
+        case_one = str(req['case_one'])
+
+        case_two = str(req['case_two'])
+
+        case_one = Case.query.filter_by(case_name=case_one).first()
+
+        case_two = Case.query.filter_by(case_name=case_two).first()
+
+        if not case_one:
+            '''
+            If case one is not present in database.
+            '''
+            return 'case one with that name does not exist', 404
+
+        if not case_two:
+            '''
+            If case two is not present in database.
+            '''
+            return 'case two with that name does not exist', 404
+
+        case_one_path = case_one.data_path
+
+        case_two_path = case_two.data_path
+
+    except Exception as e:
+
+        print(e)
+
+        return 'Please provide valid Case', 400
+
+    case_one_path = os.sep.join([case_one_path, PATH_TO_BROWSER])
+    case_two_path = os.sep.join([case_two_path, PATH_TO_BROWSER])
+
+    history_list = get_common_browser_history(case_one_path, case_two_path)
+
+    if history_list:
+        return jsonify(history_list)
 
     else:
         return "No Data Found", 404
