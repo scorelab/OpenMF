88f9165e29193eca171975532126458ee0d00627
sachin <sachinsom507@gmail.com>
2021-08-08 10:01:41 -0700

    [Backend]: Created Route for sending and verifying email.

--- flask-backend/api/helpers/mail.py
+++ flask-backend/api/helpers/mail.py
@@ -34,3 +34,34 @@ def send_reset_link_mail(recipient, reset_link):
     except Exception as e:
         print(f"MAIL ERROR: {e}")
         raise Exception
+
+
+def send_verify_link_mail(recipient, verify_link):
+    """
+    Function to send email verify link through mail.
+    Parameters:
+    1. recipiant: Email address of recipient.
+    2. reset_link: Reset Link for changing password.
+
+    Result:
+    returns none
+    """
+
+    # create message instance
+    message = Message("OpenMF: Email Verification Link", sender='test@openmf.com', recipients=[recipient])
+
+    # Create message body
+    message.body = f"Use this link to verify your email: {verify_link}"
+
+    # send message
+    try:
+        mail.send(message)
+
+    # Handle error
+    except ConnectionRefusedError as e:
+        print(f"MAIL ERROR: {e}")
+        raise ConnectionRefusedError
+
+    except Exception as e:
+        print(f"MAIL ERROR: {e}")
+        raise Exception
