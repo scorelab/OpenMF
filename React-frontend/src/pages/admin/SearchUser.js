68010aaf99431894ac1a55797fa30b59e816fb81
safwan20 <safwanmansuri.ict17@gmail.com>
2021-11-14 21:12:18 +0530

    search user page

--- React-frontend/src/pages/admin/SearchUser.js
+++ React-frontend/src/pages/admin/SearchUser.js
@@ -1,20 +1,19 @@
 /*
-* Component to display all the members of an admin.
+* Component to search the members.
 */
 
 import React, { useEffect, useState } from 'react'
 import Layout from '../../components/core/Layout'
-import ShowMembers from '../../components/Admin/ShowMembers';
+import SearchMembers from '../../components/Admin/SearchMembers';
 import HomeLogo from '../../components/core/HomeLogo';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMembers } from '../../store/actions/admin';
-import { set } from 'date-fns';
 
 const SearchUser = () => {
     // auth reducer
     const auth = useSelector(state => state.auth)
     const dispatch = useDispatch()
-    const {extractors, managements, isLoading} = useSelector(state => state.admin)
+    const { extractors, managements, isLoading } = useSelector(state => state.admin)
 
     useEffect(() => {
         dispatch(fetchMembers())
@@ -24,7 +23,12 @@ const SearchUser = () => {
         return (
             <div>
                 <Layout sidebarBool={true}>
-                    <ShowMembers extractors={extractors} managements={managements} isLoading={isLoading} isSearch={true}/>
+                    <SearchMembers 
+                      extractors={extractors} 
+                      managements={managements}
+                      isLoading={isLoading}
+                      isSearch={true}
+                />
                 </Layout>
             </div>
         )
