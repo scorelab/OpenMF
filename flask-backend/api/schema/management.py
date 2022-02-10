127a44b7a2474a4fc721bf864f19f5fff41f56bf
Prerak Mathur <mathur.prerak@gmail.com>
2022-02-10 13:37:29 +0530

    Repaired management.py

--- flask-backend/api/schema/management.py
+++ flask-backend/api/schema/management.py
@@ -10,4 +10,4 @@ class ManagementSchema(ma.Schema):
     Schema for admin model
     """
     class Meta:
-        fields = ('name', 'email', 'public_id', 'vefified', 'created_on', 'updated_on', 'role')
+        fields = ('name', 'email', 'public_id', 'verified', 'created_on', 'updated_on', 'role')
