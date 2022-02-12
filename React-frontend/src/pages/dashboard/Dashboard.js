b1d403cda2f98d45c1516383b4b30c3ebcb3521b
Prerak Mathur <mathur.prerak@gmail.com>
2022-02-12 07:27:28 +0530

    Navbar Pages Added/Linked

---
+++ React-frontend/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+/*
+* Dashboard Page Component
+*/
+
+// Import Dependecies
+import React from 'react';
+import Layout from '../../components/core/Layout';
+
+
+
+// Main Profile Component
+const AboutPage = () => {
+    return (
+        <Layout sidebarBool={true}>
+            <h1>Dashboard</h1>
+        </Layout>
+    )
+}
+
+export default AboutPage
