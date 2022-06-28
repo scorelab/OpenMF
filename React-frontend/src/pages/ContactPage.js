5c3fafe8eed19c5b154aad0706501d3072a4ab26
Prerak Mathur <mathur.prerak@gmail.com>
2022-06-28 15:22:19 +0530

    Contact Page Modified

--- React-frontend/src/pages/ContactPage.js
+++ React-frontend/src/pages/ContactPage.js
@@ -5,13 +5,15 @@
 // Import Dependecies
 import React from 'react';
 import Layout from '../components/core/Layout';
+import ContactForm from '../components/core/ContactForm';
 
 
 // Main AboutPage Component
 const AboutPage = () => {
     return (
-        <Layout sidebarBool={true}>
+        <Layout sidebarBool={false}>
             <h1>Contact page</h1>
+            <ContactForm />
         </Layout>
     )
 }
