483be9162ca51b10390c44d80b9eb99550e327c5
Prerak Mathur <mathur.prerak@gmail.com>
2022-08-21 05:24:32 +0530

    Dashboard Completed

--- React-frontend/src/pages/dashboard/Dashboard.js
+++ React-frontend/src/pages/dashboard/Dashboard.js
@@ -4,24 +4,30 @@
 // Import Dependecies
 import React from 'react';
 import Layout from '../../components/core/Layout';
+import ManagementLayout from '../../components/Management/ManagementLayout';
+import ExtractorLayout from '../../components/Extractor/ExtractorLayout';
 import ProfileCard from './ProfileCard';
 import { makeStyles } from '@material-ui/core';
 import {
     Container,
     Typography,
-    Box
+    Box,
+    Grid
 } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import TaskCard from '../../components/Admin/TaskCard';
+
 
 // custom styles
 const useStyle = makeStyles((theme) => ({
     root: {
         marginTop: '10vh',
         height: '82.5vh',
+        width: '100%',
         padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start'
+        margin: '20px'
     },
     TaskList: {
         display: 'flex',
@@ -31,20 +37,124 @@ const useStyle = makeStyles((theme) => ({
 }))
 
 const Dashboard = () => {
+    // custom styles
     const classes = useStyle()
-    return (
-        <Layout sidebarBool={true}>
-            <Container component="main" className={classes.root}>
-                <Typography component="h1" variant="h5">
-                    Dashboard
-                </Typography>
-                <Box component="div" className={classes.TaskList}>
-                    <ProfileCard />
-                </Box>
-            </Container>
-        </Layout>
 
-    )
+    // get tasks from admin reducer
+    const auth = useSelector(state => state.auth)
+
+    // fetch tasks
+    const tasksAdmin = useSelector(state => state.admin.tasks)
+    const tasksManagement = useSelector(state => state.management.todoTasks)
+    const tasksExtractor = useSelector(state => state.management.todoTasks)
+
+    // If the role is admin
+    if (auth.user.role === 'admin') {
+        return (
+            <Layout sidebarBool={true}>
+                <Container component="main" className={classes.root}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h4" component="h1" gutterBottom>
+                                Dashboard
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <ProfileCard />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box>
+                                {
+                                    (tasksAdmin.length > 0) ?
+                                        tasksAdmin.map((tasksAdmin) => {
+                                            return (<TaskCard {...tasksAdmin} key={tasksAdmin.id} />)
+
+                                        })
+                                        : (
+                                            <span> Not Tasks Found.</span>
+                                        )
+                                }
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Layout>
+        )
+    }
+
+    // If the role is management
+    else if (auth.user.role === 'management') {
+        return (
+            <ManagementLayout sidebarBool={true}>
+                <Container component="main" className={classes.root}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h4" component="h1" gutterBottom>
+                                Dashboard
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={5} >
+                        <Grid item xs={12} sm={12} md={6}>
+                            <ProfileCard />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box>
+                                {
+                                    (tasksManagement.length > 0) ?
+                                        tasksManagement.map((tasksManagement) => {
+                                            return (<TaskCard {...tasksManagement} key={tasksManagement.id} />)
+
+                                        })
+                                        : (
+                                            <span> Not Tasks Found.</span>
+                                        )
+                                }
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </ManagementLayout>
+        )
+    }
+
+    // If the role is extractor
+    else if (auth.user.role === 'extractor') {
+        return (
+            <ExtractorLayout sidebarBool={true}>
+                <Container component="main" className={classes.root}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h4" component="h1" gutterBottom>
+                                Dashboard
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={5} >
+                        <Grid item xs={12} sm={12} md={6}>
+                            <ProfileCard />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box>
+                                {
+                                    (tasksExtractor.length > 0) ?
+                                        tasksExtractor.map((tasksExtractor) => {
+                                            return (<TaskCard {...tasksExtractor} key={tasksExtractor.id} />)
+
+                                        })
+                                        : (
+                                            <span> Not Tasks Found.</span>
+                                        )
+                                }
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </ExtractorLayout>
+        )
+    }
 }
 
 export default Dashboard
